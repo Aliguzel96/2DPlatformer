deeba262374ef531dde0669b10ef259bba9a7738
Aliguzel96 <aligzl1996@gmail.com>
2022-10-10 11:24:57 +0300

    Düşmana ölüm animasyonu eklendi
    Düşman ölünce ortaya çıkan bir efekt eklendi.
    Particle sistemi için efekt materyali oluşturma: Materyal Shader uzantısı; Legacy - Particles - Alpha Blended
     İligli ayarlamalar yapıldıktan sonra texture kısmına istenilen sprite verilir

--- Assets/Scripts/arrowController.cs
+++ Assets/Scripts/arrowController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class arrowController : MonoBehaviour
 {
+
+    [SerializeField] GameObject effect;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!(collision.gameObject.tag == "Player"))//çarptýðý nesne player deðilse
@@ -13,6 +16,7 @@ public class arrowController : MonoBehaviour
                 if(collision.gameObject.CompareTag("Enemy"))//çarptýðý nesne düþman ise
                     {
                         Destroy(collision.gameObject); //çarptýðý düsmaný yok et
+                        Instantiate(effect, collision.gameObject.transform.position, Quaternion.identity);//ölünce efekt çýksýn
                     }
         }
     }
